22b21b14eb9e23abd075d08d4d7ebff5d8a824d8
PSA-VN-LT060\Truong Viet Thinh <t8.truong@playstudios.asia>
2023-03-09 14:32:06 +0700

    Convert pdf to text

--- Form1.cs
+++ Form1.cs
@@ -1,49 +1,95 @@
-using System.Text;
 using ImageMagick;
+using IronOcr;
 
 namespace SearchInPDF
 {
     public partial class Form1 : Form
     {
+        private List<string> filePaths;
+        private List<string> searchStrings;
+        private Dictionary<string, string> mappingFileAndContent;
+        private Dictionary<string, List<string>> missingFileAndContent;
+
         public Form1()
         {
             InitializeComponent();
+            filePaths = new List<string>();
+            searchStrings = new List<string>();
+            mappingFileAndContent = new Dictionary<string,string>();
+            missingFileAndContent = new Dictionary<string, List<string>>();
         }
 
-        private void SetText(string text)
+        private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = text;
+            searchStrings.AddRange(textBox2.Text.Split(","));
+            ReadPdfContent();
+
+            foreach (var file in mappingFileAndContent)
+            {
+                var missingString = new List<string>();
+                foreach (var searchString in searchStrings)
+                {
+                    if (!file.Value.Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        missingString.Add(searchString);
+                    }
+                }
+
+                if (missingString.Any())
+                {
+                    missingFileAndContent.Add(file.Key, missingString);
+                }
+            }
+
+            foreach (var missingContent in missingFileAndContent)
+            {
+                listView1.Items.Add(Path.GetFileName(missingContent.Key));
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                try
-                {
-                    foreach (var fileName in openFileDialog1.FileNames)
-                    {
-                        listView1.Items.Add(fileName);
-                        using (MagickImage image = new MagickImage())
-                        {
-                            var readSettings = new MagickReadSettings();
-                            readSettings.Density = new Density(300);
-                            readSettings.Compression = CompressionMethod.NoCompression;
+                filePaths.AddRange(openFileDialog1.FileNames);
+            }
+        }
 
-                            image.Read(fileName,readSettings);
-                            image.Sharpen(Channels.Black);
-                            image.Density = new Density(300);
-                            image.Quality = 100;
-                            image.Format = MagickFormat.Png;
-                            image.Write("./testImg.png");
-                        }
+        private void ReadPdfContent()
+        {
+            try
+            {
+                foreach (var fileName in filePaths)
+                {
+                    using (MagickImage image = new MagickImage())
+                    {
+                        var readSettings = new MagickReadSettings();
+                        readSettings.Density = new Density(300);
+                        readSettings.Compression = CompressionMethod.NoCompression;
+
+                        image.Read(fileName, readSettings);
+                        image.Sharpen(Channels.Black);
+                        image.Density = new Density(300);
+                        image.Quality = 100;
+                        image.Format = MagickFormat.Png;
+                        image.Write("./temp.png");
+                    }
+
+                    // convert image to text
+                    var Ocr = new IronTesseract();
+                    Ocr.Language = OcrLanguage.English;
+                    using (var input = new OcrInput())
+                    {
+                        input.AddImage(@"./temp.png");
+                        var result = Ocr.Read(input);
+                        mappingFileAndContent.Add(fileName, result.Text);
                     }
                 }
-                catch (Exception exception)
-                {
-                    Console.WriteLine(exception);
-                    throw;
-                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
             }
         }
     }
